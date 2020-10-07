eb6e177c31c71a25a802b5a93a6c0ab3b7619a11
Nadav <nadavro1@gmail.com>
2020-10-07 17:03:29 +0300

    commit

--- index.js
+++ index.js
@@ -12,9 +12,13 @@ app.get('/',(req,res)=>{
     res.send("API running")
 })
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+}
 app.use('/api/users',require('./Routes/API/users'));
 app.use('/api/messages',require('./Routes/API/messages'));
 app.use('/api/auth',require('./Routes/API/auth'));
+
 
 
 const PORT= process.env.PORT || 4000;

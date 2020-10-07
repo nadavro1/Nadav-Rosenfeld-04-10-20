84c474da9b397c33991ef42eae0167976618898b
Nadav <nadavro1@gmail.com>
2020-10-07 18:26:43 +0300

    commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const path= require('path')
 const app = express();
 const connectDB= require('./config/db') 
 
@@ -12,14 +12,19 @@ app.get('/',(req,res)=>{
     res.send("API running")
 })
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
-}
+
 app.use('/api/users',require('./Routes/API/users'));
 app.use('/api/messages',require('./Routes/API/messages'));
 app.use('/api/auth',require('./Routes/API/auth'));
 
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'))
 
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    });
+}
 
 const PORT= process.env.PORT || 4000;
 
